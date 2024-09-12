21812a3fdd4aff378c9fe5120d441b60e5e5d2e4
MUHDIKYASSIDIQI9 <139628962+MUHDIKYASSIDIQI9@users.noreply.github.com>
2024-09-12 13:57:28 +0800

    Mendeteksi Bilangan ganjil dan genap.py

--- Mendeteksi Bilangan ganjil dan genap.py
+++ Mendeteksi Bilangan ganjil dan genap.py
@@ -1,7 +1,8 @@
-def deteksi ganjil_genap():
+def deteksi_ganjil_genap():
   n=int(input("masukan bilangan:")) 
   if n % 2 == 0:
     print(f"(n) adalah bilangan genap")
   else:
     print(f"(n) adalah bilangan ganjil) 
 
+deteksi_ganjil_genap() 
