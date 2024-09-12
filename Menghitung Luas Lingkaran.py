1f46a5c074e49e9c4c1658310e7619cbfe3e94c0
MUHDIKYASSIDIQI9 <139628962+MUHDIKYASSIDIQI9@users.noreply.github.com>
2024-09-12 14:04:06 +0800

    Create Menghitung Luas Lingkaran.py

---
+++ Menghitung Luas Lingkaran.py
@@ -0,0 +1,14 @@
+import math 
+
+def hitung_luas_lingkaran() :
+  r = float(input("masukan jari-jari lingkaran :")) 
+  luas math.pi***
+
+def hitung luas_lingkaran():
+
+r = float(input("Masukkan jari-jari lingkaran: "))
+
+luas math.pi *r ** 2
+print(f"Luas lingkaran: {luas:.2f}") 
+
+hitung_luas_lingkaran() 
