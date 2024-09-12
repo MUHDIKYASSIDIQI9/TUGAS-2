e29aad02c1e9c206686e7ad78f1f1376bc1feccc
MUHDIKYASSIDIQI9 <139628962+MUHDIKYASSIDIQI9@users.noreply.github.com>
2024-09-12 19:55:51 +0800

    Penjumlahan.py

--- Penjumlahan.py
+++ Penjumlahan.py
@@ -1,6 +1,7 @@
 def jumlahkan_dua_bilangan():
   a=float(input("masukan bilangan pertama")) 
   b=float(input("masukan bilangan kedua")) 
+  hasil = a + b
   print("hasil penjumlahan:", hasil) 
 
 jumlahkan_dua_bilangan() 
